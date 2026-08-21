4723da1136758c679b898b9bf9848d36c67dee18
Aryan kumar <aryankumar23947@gmail.com>
2026-08-21 16:01:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const btnsEle = document.querySelectorAll("button")
+const inputEle = document.getElementById("result")
+
+for(let i = 0; i<btnsEle.length; i++){
+    btnsEle[i].addEventListener("click", ()=>{
+        // console.log(btnsEle[i].textContent);
+        const btnValue = btnsEle[i].textContent
+        if(btnValue === "C"){
+            clearResult()
+        }
+        else if(btnValue === "="){
+            calculateResult()
+        }else{
+            appendValue(btnValue)
+        }
+    })
+}
+function clearResult(){
+    inputEle.value = ""
+}
+function calculateResult(){
+    inputEle.value = eval(inputEle.value)
+}
+function appendValue(btnValue){
+    inputEle.value += btnValue
+}
